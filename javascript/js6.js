acf75678565d69a4e62e178bac6993774e04423b
mekeyace3-jpg <mekeyace3@gmail.com>
2025-11-07 17:20:17 +0900

    main
    2025-11-07

---
+++ javascript/js6.js
@@ -0,0 +1,12 @@
+//console.log("연습!");
+
+var user = "강감찬";
+var msg = user + "님 환영합니다. 포인트는 " + point;
+console.log(msg);
+
+/*
+var sum = data + data2;
+console.log(sum);
+*/
+var arr = "오늘의 공지사항 [2025-11-06]";
+var arr2 = "시스템 점검 [2025-11-06]";
---
+++ javascript/result.js
@@ -0,0 +1,7 @@
+//level 변수 생성하여 데이터 부분은 data.js에서 활성화 하여 변수값을 받아서 처리
+if(level == ""){
+    console.log("해당 회원 정보가 확인 되지 않습니다.");
+}
+else {
+    console.log(mid + "님 등급은 :" + level);
+}
